d90e5ea8f352e1fdf0d43309aa5d7bd90627087b
rainfordays <sebb.tornquist@gmail.com>
2020-01-29 14:58:06 +0100

    Update AutoOpen.lua

--- AutoOpen.lua
+++ AutoOpen.lua
@@ -36,23 +36,24 @@ function E:BAG_UPDATE(B)
   for S = 1, GetContainerNumSlots(B) do
     local _, _, locked, _, _, lootable, itemLink = GetContainerItemInfo(B, S)
 
-    if itemLink and not string.find(itemLink, "Lockbox") and not string.find(itemLink, "Junkbox") then
-      if lootable and not locked then
+    if itemLink and not string.find(itemLink, "Lockbox") and not string.find(itemLink, "Junkbox") then -- make sure its not a lockbox
+      if lootable and not locked then -- item is lootable and not locked by server
         local autolootDefault = GetCVar("autoLootDefault")
-        if IsModifiedClick(AUTOLOOTTOGGLE) and autolootDefault then
+        
+        if autolootDefault then -- autolooting
+          if IsModifiedClick(AUTOLOOTTOGGLE) then -- currently holding autoloot mod key
+            SetCVar("autoLootDefault", 0) -- swap the autoloot behaviour so it autoloots even with mod key held
+            UseContainerItem(B, S)
+            SetCVar("autoLootDefault", 1) -- swap back
+          else -- not holding autoloot mod key
+            UseContainerItem(B, S)
+          end
+        else -- not autolooting
+          SetCVar("autoLootDefault", 1)
+          UseContainerItem(B, S)
           SetCVar("autoLootDefault", 0)
-          UseContainerItem(B, S)
-          SetCVar("autoLootDefault", 1)
-
-        elseif autolootDefault then
-          UseContainerItem(B, S)
-
-        else
-          SetCVar("autoLootDefault", 1)
-          UseContainerItem(B, S)
-          SetCVar("autoLootDefault", autolootDefault)
-
         end
+        
       end
     end
   end

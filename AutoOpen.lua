2454aee8b16c7a75be69ae4c7112ade360885878
rainfordays <sebb.tornquist@gmail.com>
2020-01-29 17:45:56 +0100

    Update

--- AutoOpen.lua
+++ AutoOpen.lua
@@ -39,7 +39,7 @@ function E:BAG_UPDATE(B)
     if itemLink and not string.find(itemLink, "Lockbox") and not string.find(itemLink, "Junkbox") then -- make sure its not a lockbox
       if lootable and not locked then -- item is lootable and not locked by server
         local autolootDefault = GetCVar("autoLootDefault")
-        
+
         if autolootDefault then -- autolooting
           if IsModifiedClick(AUTOLOOTTOGGLE) then -- currently holding autoloot mod key
             SetCVar("autoLootDefault", 0) -- swap the autoloot behaviour so it autoloots even with mod key held
@@ -53,7 +53,7 @@ function E:BAG_UPDATE(B)
           UseContainerItem(B, S)
           SetCVar("autoLootDefault", 0)
         end
-        
+
       end
     end
   end
